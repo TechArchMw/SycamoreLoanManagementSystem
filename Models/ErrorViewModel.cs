1b055f0ee3703b6529825f3916b10f7b9bc1739f
Denzel <denzelmac8fantasy@gmail.com>
2021-06-22 15:44:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SycamoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
