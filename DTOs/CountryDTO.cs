f4e4bb21d3ecebd21a19be9f12515314d5b4c022
matewere william <sekulu1995@outlook.com>
2021-06-29 05:38:32 +0200

    Added Countries UI

---
+++ DTOs/CountryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SycamoreWebApp.DTOs
+{
+    public class CountryDTO
+    {
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string NumericCode { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
